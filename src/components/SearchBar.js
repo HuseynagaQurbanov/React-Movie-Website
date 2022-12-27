f163660ddd54c44254a75e27aa104bb8c41d18f5
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2022-12-27 17:07:27 +0400

    Movies added by using 'map' function

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class SearchBar extends React.Component{
+    render(){
+        return (
+            <div className="row">
+                <div className="col-lg-12">
+                    <div>Search</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
 
 import App from "./components/App";
 

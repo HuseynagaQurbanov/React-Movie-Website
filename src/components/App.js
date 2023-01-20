a101961c6a9d879de7022c52acbf2c9aa4f75d37
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2023-01-20 14:37:36 +0400

    Real api added (bonus)

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
-import Axios from "axios";
 import axios from "axios";
+
+
+const apiKey = "52880d7424f9350a289d7f699b9336f1";
+const sessionId = "0229807dcfad10aa3aa4b8bded8a53fcdc2801ca";
+
 
 class App extends React.Component {
   state = {
@@ -25,8 +29,9 @@ class App extends React.Component {
   //AXIOS API COMPONENTDIDMOUNT
 
   async componentDidMount() {
-    const response = await axios.get("http://localhost:3002/movies");
-    this.setState({ movies: response.data });
+    const response = await axios.get(`https://api.themoviedb.org/3/list/8236509?api_key=${apiKey}&language=en-US`);
+    console.log(response.data.items);
+    this.setState({ movies: response.data.items });
   }
 
 
@@ -51,7 +56,7 @@ class App extends React.Component {
   //AXIOS API DELETEMOVIE
 
   deleteMovie = async (movie) => {
-    await axios.delete(`http://localhost:3002/movies/${movie.id}`)
+    await axios.post(`https://api.themoviedb.org/3/list/8236509/remove_item?media_id=${movie.id}&session_id=${sessionId}&api_key=${apiKey}`)
 
     const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
@@ -68,7 +73,7 @@ class App extends React.Component {
   render() {
     let filteredMovies = this.state.movies.filter((movie) => {
       return (
-        movie.name
+        movie.title
           .toLocaleLowerCase()
           .indexOf(this.state.searchQuery.toLocaleLowerCase()) !== -1
       );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,17 @@
 import React from "react";
 
+
+const tmdbBasePath = "https://www.themoviedb.org/t/p/w220_and_h330_face/"
+
 const MovieList = (props) => {
   return (
     <div className="row">
       {props.movies.map((movie) => (
         <div className="col-lg-3" key={movie.id}>
           <div className="card mb-4 shadow-sm">
-            <img src={movie.imageURL} className="cart-img-top" alt="" />
+            <img src={tmdbBasePath + movie.poster_path} className="cart-img-top" alt="" />
             <div className="card-body">
-              <h5 className="card-title">{movie.name}</h5>
+              <h5 className="card-title">{movie.title}</h5>
               <p className="card-text">{movie.overview}</p>
               <div className="d-flex justify-content-between align-items-center">
                 <button
@@ -21,7 +24,7 @@ const MovieList = (props) => {
                   Delete
                 </button>
                 <h2>
-                  <span className="badge badge-info">{movie.rating}</span>
+                  <span className="badge badge-info">{movie.vote_average}</span>
                 </h2>
               </div>
             </div>
